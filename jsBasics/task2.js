e4cea7610750810c182ce9dfb7726d8c8a393404
Marina Latysh <marinalatysh@MacBook-Air-Marina.local>
2025-02-17 14:37:55 +0200

    Homework 4.1

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let person1 = "Mарина";
+let person2 = "Анна";
+
+let greeting1 = "Привіт, " + person1 + " та " + person2 + "!";
+console.log(greeting1);
+
+let greeting2 = `Привіт, ${person1} та ${person2}!`;
+console.log(greeting2);
---
+++ jsBasics/task3.js
@@ -0,0 +1,6 @@
+let age = 28;
+let isAdult = age >= 18; 
+
+console.log(`Вік персони: ${age}`);
+console.log(`Чи повнолітня: ${isAdult}`
+);
